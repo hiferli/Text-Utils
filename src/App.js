46348bf219ed2e2e64269e3d170ed5808b3796a0
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-15 00:32:22 +0530

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
 	return (
 		<>
 			<Navbar title="TextUtils"></Navbar>
 			<div className="container my-3">
-				<TextForm heading = "Enter the Text:"></TextForm>
+				{/* <TextForm heading = "Enter the Text:"></TextForm> */}
+				<About></About>
 			</div>
 		</>
 	);
