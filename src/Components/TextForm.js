12ff5d3e6ecadd1f196e0a7e6dd7f4b06bbb40e7
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-15 00:06:51 +0530

    Adding Capitalized String function

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
+;
+
 export default function TextForm(props) {
     const handleUpperCaseClick = () => {
         // console.log("This Upper Case Button was Clicked")
@@ -12,6 +16,16 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    
+    const handleClearText = () => {
+        setText("");
+    }
+
+    const handleCapitalCaseClick = () => {
+        // console.log("This Upper Case Button was Clicked")
+        let newText = capitalize(text);
+        setText(newText);
+    }
 
     const handleOnChange = (event) => {
         // console.log("UpperCase button clicked");
@@ -21,19 +35,24 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter Your Text Here")
     return (
         <>
-            <div className='container'>
+            <div className='container '>
                 <h1>{props.heading}</h1>
+                
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick}>UpperCase Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowerCaseClick}>LowerCase Text</button>
+                <div className="container position-relative">
+                    <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick}>UpperCase Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleLowerCaseClick}>LowerCase Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCapitalCaseClick}>Capitalize Text</button>
+                    <button className="btn btn-primary btn-info mx-1 position-absolute top-50 end-0 translate-middle-y" onClick={handleClearText}>Clear</button>
+                </div>
             </div>
 
             <div className="container my-3">
                 <h2>Text Analytics:</h2>
-                <p>{text.split(" ").length} Words and {text.length} Characters</p>
-                <p>Would take roughly {0.08 * text.split(" ").length} Minutes to read.</p>
+                <p>{text.split(" ").length - 1} Words and {text.length} Characters</p>
+                <p>Would take roughly {0.08 * (text.split(" ").length - 1)} Minutes to read.</p>
                 <h2>Text Preview:</h2>
                 <p>{text}</p>
             </div>
