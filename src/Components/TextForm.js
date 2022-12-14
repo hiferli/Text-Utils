dfcb1fb5ef639011f9d39c5939b43c808dfd7e03
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-14 20:19:38 +0530

    Adding LowerCase Button

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,9 +1,15 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpperCaseClick = () => {
         // console.log("This Upper Case Button was Clicked")
         let newText = text.toUpperCase();
+        setText(newText);
+    }
+    
+    const handleLowerCaseClick = () => {
+        // console.log("This Upper Case Button was Clicked")
+        let newText = text.toLowerCase();
         setText(newText);
     }
 
@@ -14,12 +20,23 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("Enter Your Text Here")
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick}>UpperCase Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowerCaseClick}>LowerCase Text</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpperCaseClick}>UpperCase Text</button>
-        </div>
+
+            <div className="container my-3">
+                <h2>Text Analytics:</h2>
+                <p>{text.split(" ").length} Words and {text.length} Characters</p>
+                <p>Would take roughly {0.08 * text.split(" ").length} Minutes to read.</p>
+                <h2>Text Preview:</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
