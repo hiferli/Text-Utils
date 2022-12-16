29d5660b8711581395b302e26fc33564ac277d71
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-17 02:08:12 +0530

    Resolving Cumulative Layout Shift (CLS)

--- src/Components/Alerts.js
+++ src/Components/Alerts.js
@@ -3,11 +3,13 @@ import capitalize from 'C:/Users/Dell/Desktop/text-utils/src/index.js';
 
 export default function Alerts(props) {
     return (
-        props.alert && <div>
-            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-                <strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
-                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
-            </div>
+        <div style={{height: '50px'}}>
+            {props.alert && <div>
+                <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                    <strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
+                    {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+                </div>
+            </div>}
         </div>
     )
 }
